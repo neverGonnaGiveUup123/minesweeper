29dfdba16ec52ad4046fee0b0b436b5897c705c8
Siddharth <st22041@ormiston.school.nz>
2023-08-01 11:39:26 +1200

    Initial commit. Literally just a board with tiles

---
+++ settings.py
@@ -0,0 +1,9 @@
+SCREEN_DIMENSIONS = (500,500)
+FPS = 30
+CAPTION = "Minesweeper"
+GRID_SIZE = 5
+MAX_NUM_OF_MINES = 15
+IMAGES = {
+    'blank_tile' : 'images/blank_tile.png',
+    'hover_tile' : 'images/blank_tile_selected.png'
+}
